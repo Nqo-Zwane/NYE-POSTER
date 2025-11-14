9f6c87aa617f36e827e63588cc5f3ec02dc07bbc
Nqo-Zwane <manqobazwane22@gmail.com>
2025-11-14 12:24:36 +0200

    revert: migrate back to local assets for better performance and cost 15m #5

--- js/sound.js
+++ js/sound.js
@@ -2,7 +2,7 @@
 class SoundManager {
   constructor() {
     this.exploreSound = new Audio(
-      'https://nye-bucket.s3.amazonaws.com/sound/mixkit-select-click-1109.wav'
+      './public/sound/mixkit-select-click-1109.wav'
     );
     this.exploreSound.volume = 0.5;
     this.exploreSound.preload = 'auto';
